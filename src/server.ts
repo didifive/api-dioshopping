37b81f1c65e4804363a1e51e3c1ee4810326ba2d
Luis Carlos <luisczdidi@gmail.com>
2022-01-30 10:27:26 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,6 @@ app.use(cors());
 app.use(express.json());
 app.use(router)
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log(`⚡️ [server]: Server is running.`);
 });
